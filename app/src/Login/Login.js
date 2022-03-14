997207e32f0c516a26279f1db4dc1f165e0ec337
Gabriela Muniz <gabrielasmunizf@gmail.com>
2022-03-14 17:33:55 -0300

    valores

--- app/src/Login/Login.js
+++ app/src/Login/Login.js
@@ -1,63 +1,73 @@
-import React from 'react';
-import {Text, TextInput, View, Button, Image,KeyboardAvoidingView,Platform} from 'react-native';
-import Logo from "../../assets/logoListei.png"
-
+import React, { useState } from "react";
+import {
+  Text,
+  TextInput,
+  View,
+  Button,
+  Image,
+  KeyboardAvoidingView,
+  Platform,
+} from "react-native";
+import Logo from "../../assets/logoListei.png";
 
 function Login() {
- 
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
+
   return (
     <KeyboardAvoidingView
       behavior={Platform.OS === "ios" ? "padding" : "height"}
-      style={{flex:1}}
+      style={{ flex: 1 }}
     >
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <View
-        style={{
-          flex: 1,
-          justifyContent: 'center',
-          alignItems: 'center',
-          backgroundColor: 'white',
-          borderRadius: 20,
-          height: 200,
-          margin:150,
-        }}>
-       <Image source={Logo} />
-        <Text>Login</Text>
-        <TextInput
-          style={{
-            height: 40,
-            borderColor: 'gray',
-            borderWidth: 1,
-            width: 200,
-            marginTop: 20,
-          }}
-          type="email"
-          placeholder="Email"
-        />
-        <TextInput
-          style={{
-            height: 40,
-            borderColor: 'gray',
-            borderWidth: 1,
-            width: 200,
-            marginTop: 20,
-          }}
-          type="password"
-          placeholder="senha"
-        />
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
         <View
           style={{
-            marginTop: 20,
-          }}>
-          <Button title="Login" color="#4682B4" />
-
+            flex: 1,
+            justifyContent: "center",
+            alignItems: "center",
+            backgroundColor: "white",
+            borderRadius: 20,
+            height: 200,
+            margin: 150,
+          }}
+        >
+          <Image source={Logo} />
+          <Text>Login</Text>
+          <TextInput
+            style={{
+              height: 40,
+              borderColor: "gray",
+              borderWidth: 1,
+              width: 200,
+              marginTop: 20,
+            }}
+            type="email"
+            placeholder="Email"
+            value={email}
+          />
+          <TextInput
+            style={{
+              height: 40,
+              borderColor: "gray",
+              borderWidth: 1,
+              width: 200,
+              marginTop: 20,
+            }}
+            type="password"
+            placeholder="senha"
+            value={senha}
+          />
+          <View
+            style={{
+              marginTop: 20,
+            }}
+          >
+            <Button title="Login" color="#4682B4" />
+          </View>
         </View>
       </View>
-    </View>
-     </KeyboardAvoidingView>
+    </KeyboardAvoidingView>
   );
 }
-
-
 
 export default Login;
